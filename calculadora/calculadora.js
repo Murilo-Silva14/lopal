6c04dd98e41bb192d43a58c40ec7ec7af46d8a17
Murilo-Silva14 <ms20101403@gmail.com>
2026-02-25 16:46:11 -0300

    LOPAL

---
+++ calculadora/calculadora.js
@@ -0,0 +1,30 @@
+function calculadora(){    
+    let resultado = 0.0;
+    let operando1;
+    let operando2;
+    let operador;
+
+    operando1 = parseFloat( prompt("digite o primeiro número: ") );
+    operando2 = parseFloat( prompt("digite o segundo número: ") );
+    operador = parseFloat( prompt("Digite uma das operações ( + - * / ): ") )
+
+    if ( operador === "+" ){
+        resultado = operando1 + operando2;
+    } else if ( operador === "-"){
+        resultado = operando1 - operando2;
+    } else if ( operador === "*" ){
+        resultado = operando1 * operando2;
+    } else if ( operador === "/" ){
+        if (operando2 == 0){
+            alert("Não é possivel dividir por zero")
+            return
+        }
+        resultado = operando1 / operando2;
+    } else {
+        alert("operador inválido!"); 
+        return
+    }
+
+    alert("resultado: " + 
+        operando1 + " " + operador + " " + operando2 + " = " + resultado);
+}
