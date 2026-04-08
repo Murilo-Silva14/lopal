5c8fd5104eca18b943b6c455260ac05eff7a337a
Murilo-Silva14 <ms20101403@gmail.com>
2026-04-08 08:47:51 -0300

    feat: array, conjunto, mapa, callback, closure

---
+++ callback/callback.js
@@ -0,0 +1,12 @@
+function callback(){
+    const minhaFunc = (num1, num2, operacao) => {
+        return operacao(num1, num2);
+    }
+
+    //operacao = (a, b) => {a = b}
+
+    const calcular = (a, b) => {return "Vai, Curíntia! " + a};
+    
+    console.log(minhaFunc(num1 = 2, num2 = 3, operacao = calcular));
+}
+callback();
